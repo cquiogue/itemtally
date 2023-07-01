f2b829c45d0a18ed1de3a11505a3dea9646396ab
cquiogue <83323770+cquiogue@users.noreply.github.com>
2023-07-01 08:07:07 -0400

    added copy button

--- script.js
+++ script.js
@@ -86,3 +86,33 @@ function openPopout() {
     `width=${width},height=${height},left=${left},top=${top}`
   );
 }
+
+function copyOutput() {
+  const outputElement = document.getElementById('output');
+  const outputTable = outputElement.querySelector('.output-table');
+
+  // Create a range object to select the text
+  const range = document.createRange();
+  range.selectNodeContents(outputTable);
+
+  // Exclude the headers from the selection
+  const headerRow = outputTable.querySelector('.header-row');
+  range.setStartAfter(headerRow);
+
+  // Add the selected text to the clipboard
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand('copy');
+
+  // Change the copy button text and disable it temporarily
+  const copyBtn = document.querySelector('.copy-btn');
+  copyBtn.innerHTML = '<i class="fas fa-check"></i> Copied';
+  copyBtn.disabled = true;
+
+  // Reset the copy button text after 2 seconds
+  setTimeout(function () {
+    copyBtn.innerHTML = '<i class="far fa-copy"></i> <span>Copy</span>';
+    copyBtn.disabled = false;
+  }, 2000);
+}
